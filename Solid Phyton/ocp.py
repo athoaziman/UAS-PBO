37da78ae327c65d816eb43532a02ff6f7b690d3b
athoaziman <115719756+athoaziman@users.noreply.github.com>
2022-12-29 20:15:09 +0700

    Create ocp.py

---
+++ Solid Phyton/ocp.py
@@ -0,0 +1,98 @@
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def get_name(self) -> str:
+        pass
+
+animals = [
+    Animal('lion'),
+    Animal('mouse')
+]
+
+def animal_sound(animals: list):
+    for animal in animals:
+        if animal.name == 'lion':
+            print('roar')
+
+        elif animal.name == 'mouse':
+            print('squeak')
+
+animal_sound(animals)
+
+animals = [
+    Animal('lion'),
+    Animal('mouse'),
+    Animal('snake')
+]
+
+def animal_sound(animals: list):
+    for animal in animals:
+        if animal.name == 'lion':
+            print('roar')
+        elif animal.name == 'mouse':
+            print('squeak')
+        elif animal.name == 'snake':
+            print('hiss')
+
+animal_sound(animals)
+
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def get_name(self) -> str:
+        pass
+
+    def make_sound(self):
+        pass
+
+
+class Lion(Animal):
+    def make_sound(self):
+        return 'roar'
+
+
+class Mouse(Animal):
+    def make_sound(self):
+        return 'squeak'
+
+
+class Snake(Animal):
+    def make_sound(self):
+        return 'hiss'
+
+
+def animal_sound(animals: list):
+    for animal in animals:
+        print(animal.make_sound())
+
+animal_sound(animals)
+
+class Discount:
+    def __init__(self, customer, price):
+        self.customer = customer
+        self.price = price
+
+    def give_discount(self):
+            if self.customer == 'fav':
+                return self.price * 0.2
+            if self.customer == 'vip':
+                return self.price * 0.4
+            
+class Discount:
+    def __init__(self, customer, price):
+        self.customer = customer
+        self.price = price
+
+    def get_discount(self):
+            return self.price * 0.2
+
+
+class VIPDiscount(Discount):
+    def get_discount(self):
+        return super().get_discount() * 2
+
+class SuperVIPDiscount(VIPDiscount):
+    def get_discount(self):
+        return super().get_discount() * 2            
