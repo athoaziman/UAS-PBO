d3e6aaea0198e4894797c117dbede4159ae80990
athoaziman <115719756+athoaziman@users.noreply.github.com>
2022-12-29 18:30:34 +0700

    Create Burung.java

---
+++ Burung.java
@@ -0,0 +1,11 @@
+class Burung extends Enemy {
+    void walk(){
+        System.out.println("Burung berjalan");
+    }
+    void jump(){
+        System.out.println("Burung loncat-loncat");
+    }
+    void fly(){
+        System.out.println("Burung Terbang...");
+    }
+}
